39a10e75a999b3e2f2666cebee148488e245fb7f
Alkan Aziz <alkanaziz@gmail.com>
2025-03-19 14:42:21 +0100

    Comment out Prisma data fetching and logging in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 
 export default async function Home() {
-  const containers = await prisma.container.findMany();
-  const companies = await prisma.company.findMany();
-  const materials = await prisma.material.findMany();
-  const transactions = await prisma.transaction.findMany();
-  const metrics = await prisma.metric.findMany();
-  const users = await prisma.user.findMany();
+  // const containers = await prisma.container.findMany();
+  // const companies = await prisma.company.findMany();
+  // const materials = await prisma.material.findMany();
+  // const transactions = await prisma.transaction.findMany();
+  // const metrics = await prisma.metric.findMany();
+  // const users = await prisma.user.findMany();
 
-  console.log("Containers:", containers);
-  console.log("Companies:", companies);
-  console.log("Materials:", materials);
-  console.log("Transactions:", transactions);
-  console.log("Metrics:", metrics);
-  console.log("Users:", users);
+  // console.log("Containers:", containers);
+  // console.log("Companies:", companies);
+  // console.log("Materials:", materials);
+  // console.log("Transactions:", transactions);
+  // console.log("Metrics:", metrics);
+  // console.log("Users:", users);
 
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
