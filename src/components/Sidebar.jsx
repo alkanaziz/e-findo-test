721de092e4747bfd9d7f8f28fec61f112ec236b3
Alkan Aziz <alkanaziz@gmail.com>
2025-03-20 17:21:27 +0100

    Update size of icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,6 @@ const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [isVisible, setIsVisible] = useState(true);
 
-  // İki aşamalı animasyon için isVisible state'ini kullan
   useEffect(() => {
     let timeout;
     if (isOpen) {
@@ -22,7 +21,7 @@ const Sidebar = () => {
     } else {
       timeout = setTimeout(() => {
         setIsVisible(false);
-      }, 300); // Animasyon süresi kadar bekle
+      }, 300);
     }
 
     return () => clearTimeout(timeout);
@@ -63,7 +62,7 @@ const Sidebar = () => {
 
   return (
     <div className="relative">
-      {/* Açma Butonu */}
+      {/* Öffnen-Schaltfläche */}
       <div
         className={`fixed left-0 top-1/2 z-10 flex h-16 -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-r-full border border-e-brown-400 bg-e-brown-400 shadow-lg transition-all duration-300 ease-in-out hover:bg-e-brown-500 dark:border-e-background-700 ${
           isOpen ? "-translate-x-full opacity-0" : "translate-x-0 opacity-100"
@@ -73,7 +72,7 @@ const Sidebar = () => {
         <IoIosArrowForward className="text-e-brown-900 h-6 w-6 rounded-r-full transition-transform duration-300 hover:translate-x-1" />
       </div>
 
-      {/* Sidebar */}
+      {/* Seitenleiste */}
       {isVisible && (
         <aside
           className={`transition-all duration-300 ease-in-out ${
@@ -82,15 +81,15 @@ const Sidebar = () => {
               : "pointer-events-none min-w-0 max-w-0 -translate-x-full opacity-0"
           } relative rounded-lg border border-e-brown-400 bg-e-background-50 shadow-lg dark:bg-e-background-800`}
         >
-          {/* Kapatma Butonu */}
+          {/* Schließen-Schaltfläche */}
           <div
             className="hidden w-full cursor-pointer rounded-t-lg bg-e-brown-400 transition-colors duration-300 hover:bg-e-brown-500 md:block"
             onClick={() => setIsOpen(false)}
           >
             <IoIosArrowBack className="text-e-brown-900 h-6 w-full rounded-t-lg shadow-sm transition-transform duration-300 hover:-translate-x-1" />
           </div>
-          {/* Menü İçeriği */}
-          <nav className="flex h-full md:h-[calc(100vh-12rem)] flex-wrap *:max-w-28 justify-evenly md:justify-between gap-3 p-3 *:rounded-lg *:border *:border-e-brown-400 md:flex-nowrap md:flex-col">
+          {/* Menüinhalt */}
+          <nav className="flex h-full flex-wrap justify-evenly gap-3 p-3 *:max-w-28 *:rounded-lg *:border *:border-e-brown-400 md:h-[calc(100vh-12rem)] md:flex-col md:flex-nowrap md:justify-between">
             {menuItems.map((item, index) => (
               <Link
                 key={index}
@@ -98,7 +97,7 @@ const Sidebar = () => {
                 className="group flex h-full w-full flex-col items-center justify-center transition-colors hover:bg-[#9F8170]"
               >
                 <div className="flex h-10 w-10 items-center justify-center">
-                  <item.icon className="h-6 w-6" />
+                  <item.icon className="size-7 md:size-10" />
                 </div>
                 <span className="mt-1 px-1 text-center text-[11px] font-medium">
                   {item.title}
