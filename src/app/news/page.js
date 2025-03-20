6138f3bdc42b4106debc4addc490f7f89055591a
Alkan Aziz <alkanaziz@gmail.com>
2025-03-20 09:22:00 +0100

    Add News and Stammdaten components with basic layout

---
+++ src/app/news/page.js
@@ -0,0 +1,9 @@
+export default function News() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-screen-xl items-center justify-center">
+      <div className="container mx-auto w-full">
+        <h1 className="text-center text-2xl font-bold">News</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/stammdaten/page.js
@@ -0,0 +1,9 @@
+export default function Stammdaten() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-screen-xl items-center justify-center">
+      <div className="container mx-auto w-full">
+        <h1 className="text-center text-2xl font-bold">Stammdaten</h1>
+      </div>
+    </div>
+  );
+}
