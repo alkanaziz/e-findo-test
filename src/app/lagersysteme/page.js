638a83b25e797280861ed42a890723b3da1ad92e
Alkan Aziz <alkanaziz@gmail.com>
2025-03-22 15:32:30 +0100

    Refactor LagersystemePage to simplify StorageSystem usage and improve layout

--- src/app/lagersysteme/page.js
+++ src/app/lagersysteme/page.js
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
-import { BiCube } from "react-icons/bi";
+import { useState } from "react";
 import StorageSystem from "../../components/StorageSystem";
 
 export default function LagersystemePage() {
@@ -10,11 +9,12 @@ export default function LagersystemePage() {
   const handleToggleFullscreen = (fullscreen) => {
     setIsFullscreen(fullscreen);
   };
-
   return (
-    <div className="container mx-auto h-full">
-      <div className={`h-[calc(100vh-180px)] ${isFullscreen ? 'h-screen w-screen fixed left-0 top-0 z-50 m-0 rounded-none' : 'relative'}`}>
-        <StorageSystem isInModal={false} isHomePage={false} onToggleFullscreen={handleToggleFullscreen} />
+    <div className="mb-2 flex w-full flex-col gap-2 overflow-auto">
+      <div
+        className={`StorageSystem ${isFullscreen ? "fixed bg-e-background-50 dark:bg-e-background-500 left-0 top-0 z-50 p-5 flex h-screen w-screen items-center justify-center rounded-none *:w-[90vw]" : ""} h-full overflow-y-auto rounded-lg shadow-sm shadow-e-brown-500/20 dark:bg-e-background-700 dark:text-gray-200 dark:shadow-none`}
+      >
+        <StorageSystem onToggleFullscreen={handleToggleFullscreen} />
       </div>
     </div>
   );
