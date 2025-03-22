151262eb74872f6bc985c1eca1f85d166185a35a
Alkan Aziz <alkanaziz@gmail.com>
2025-03-23 00:27:57 +0100

    Add Chart components to Home for Erlöse and Gewicht trends with fullscreen toggle

--- src/app/page.js
+++ src/app/page.js
@@ -3,28 +3,68 @@
 import { useState, useEffect } from "react";
 import DashboardStats from "@/components/DashboardStats";
 import StorageSystem from "@/components/StorageSystem";
-
+import Chart from "@/components/Chart";
 export default function Home() {
   const [isStorageFullscreen, setIsStorageFullscreen] = useState(false);
+  const [isErloesChartFullscreen, setIsErloesChartFullscreen] = useState(false);
+  const [isGewichtChartFullscreen, setIsGewichtChartFullscreen] =
+    useState(false);
 
   const handleStorageToggleFullscreen = (isFullscreen) => {
     setIsStorageFullscreen(isFullscreen);
   };
 
+  const handleChartToggleFullscreen = (isFullscreen, type) => {
+    if (type === "erloese") {
+      setIsErloesChartFullscreen(isFullscreen);
+    } else {
+      setIsGewichtChartFullscreen(isFullscreen);
+    }
+  };
+
   return (
-    <main className="dashboardStats flex flex-col overflow-auto gap-2 md:w-full">
-      {!isStorageFullscreen && <DashboardStats />}
-      <div className={`StorageSystem ${isStorageFullscreen ? 'h-[calc(100vh-6rem)]' : 'max-h-[33vh]'} overflow-y-auto rounded-lg bg-e-background-50 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-700 dark:text-gray-200 dark:shadow-none`}>
-        <StorageSystem onToggleFullscreen={handleStorageToggleFullscreen} />
-      </div>
+    <main className="dashboardStats flex flex-col gap-2 overflow-auto md:w-full">
+      {!isStorageFullscreen &&
+        !isErloesChartFullscreen &&
+        !isGewichtChartFullscreen && <DashboardStats />}
+      {!isErloesChartFullscreen && !isGewichtChartFullscreen && (
+        <div
+          className={`StorageSystem ${isStorageFullscreen ? "h-[calc(100vh-6rem)]" : "max-h-[33vh]"} overflow-y-auto rounded-lg bg-e-background-50 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-700 dark:text-gray-200 dark:shadow-none`}
+        >
+          <StorageSystem onToggleFullscreen={handleStorageToggleFullscreen} />
+        </div>
+      )}
       {!isStorageFullscreen && (
-        <div className="flex h-[32vh] flex-col gap-2 rounded-lg bg-e-background-50 p-4 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-700 dark:shadow-none md:flex-row">
-          <div className="RevenueChart rounded-lg bg-e-white p-4 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-800 dark:text-gray-200 dark:shadow-none md:w-1/2">
-            Erlöse
-          </div>
-          <div className="WeightTrend rounded-lg bg-e-white p-4 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-800 dark:text-gray-200 dark:shadow-none md:w-1/2">
-            Gewichtstrend
-          </div>
+        <div
+          className={`${isErloesChartFullscreen || isGewichtChartFullscreen ? "h-[calc(100vh-6rem)]" : "h-full"} flex flex-col gap-2 rounded-lg shadow-sm shadow-e-brown-500/20 dark:shadow-none lg:flex-row`}
+        >
+          {!isGewichtChartFullscreen && (
+            <div
+              className={`Chart bg-e-white relative h-full rounded-lg border border-e-brown-300 bg-e-background-50 p-1 pt-5 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-800 dark:text-gray-200 dark:shadow-none xl:pt-0 ${isErloesChartFullscreen ? "w-full" : "lg:w-1/2"}`}
+            >
+              <Chart
+                onToggleFullscreen={(isFullscreen) =>
+                  handleChartToggleFullscreen(isFullscreen, "erloese")
+                }
+                type="erloese"
+              />
+            </div>
+          )}
+          {!isErloesChartFullscreen && (
+            <div
+              className={`Chart bg-e-white relative h-full rounded-lg border border-e-brown-300 bg-e-background-50 p-1 pt-5 shadow-sm shadow-e-brown-500/20 dark:bg-e-background-800 dark:text-gray-200 dark:shadow-none xl:pt-0 ${isGewichtChartFullscreen ? "w-full" : "lg:w-1/2"}`}
+            >
+              <Chart
+                onToggleFullscreen={(isFullscreen) =>
+                  handleChartToggleFullscreen(
+                    isFullscreen,
+                    "gewichtsentciklung",
+                  )
+                }
+                type="gewichtsentciklung"
+              />
+            </div>
+          )}
         </div>
       )}
     </main>
